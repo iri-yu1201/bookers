f4b52ef872e488366ad16dc7340a31230b3d46ea
iri-yu1201 <iri12yuta01@gmail.com>
2021-05-05 12:09:12 +0000

    [Update]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/show'
-  get 'books/edit'
-  get 'books/new'
-  get 'books/index'
+  resources :books
+  
+  root :to => 'homes#top'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
